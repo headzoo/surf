d20566af66a77dd99cb0d9b55094e13d35f71954
headzoo <sean@headzoo.io>
2014-08-27 18:28:21 -0400

    Initial import util package

---
+++ util/util.go
@@ -0,0 +1,13 @@
+package util
+
+import "os"
+
+// FileExists returns if the given file exists.
+func FileExists(file string) bool {
+	if _, err := os.Stat(file); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
