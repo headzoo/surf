5f16a081095779d0ec73db05675eea169e43b026
headzoo <sean@headzoo.io>
2014-08-27 04:41:13 -0400

    Merged link.go into document.go

--- element/document.go
+++ element/document.go
@@ -87,6 +87,18 @@ type Browsable interface {
 	Write(o io.Writer) (int, error)
 }
 
+// Link stores the properties of a page link.
+type Link struct {
+	// ID is the value of the id attribute or empty when there is no id.
+	ID string
+
+	// Href is the value of the href attribute.
+	Href string
+
+	// Text is the text appearing between the opening and closing anchor tag.
+	Text string
+}
+
 // Page represents a web page document.
 type Page struct {
 	resp *http.Response
