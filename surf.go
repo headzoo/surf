b6103204a4d62e545d11db808c12d8ddcde2d11f
headzoo <sean@headzoo.io>
2014-08-28 03:08:25 -0400

    Moved default attributes into surf package

--- surf.go
+++ surf.go
@@ -2,22 +2,37 @@
 package surf
 
 import (
+	"github.com/headzoo/surf/agent"
 	"github.com/headzoo/surf/browser"
 	"github.com/headzoo/surf/jar"
+)
+
+var (
+	// DefaultUserAgent is the global user agent value.
+	DefaultUserAgent = agent.Create()
+
+	// DefaultSendRefererAttribute is the global value for the AttributeSendReferer attribute.
+	DefaultSendReferer = true
+
+	// DefaultMetaRefreshHandlingAttribute is the global value for the AttributeHandleRefresh attribute.
+	DefaultMetaRefreshHandling = true
+
+	// DefaultFollowRedirectsAttribute is the global value for the AttributeFollowRedirects attribute.
+	DefaultFollowRedirects = true
 )
 
 // NewBrowser creates and returns a *browser.Browser type.
 func NewBrowser() *browser.Browser {
 	bow := &browser.Browser{}
-	bow.SetUserAgent(browser.DefaultUserAgent)
+	bow.SetUserAgent(DefaultUserAgent)
 	bow.SetCookieJar(jar.NewMemoryCookies())
 	bow.SetBookmarksJar(jar.NewMemoryBookmarks())
 	bow.SetHistoryJar(jar.NewMemoryHistory())
 	bow.SetHeaders(jar.NewMemoryHeaders())
 	bow.SetAttributes(browser.AttributeMap{
-		browser.SendReferer:         browser.DefaultSendReferer,
-		browser.MetaRefreshHandling: browser.DefaultMetaRefreshHandling,
-		browser.FollowRedirects:     browser.DefaultFollowRedirects,
+		browser.SendReferer:         DefaultSendReferer,
+		browser.MetaRefreshHandling: DefaultMetaRefreshHandling,
+		browser.FollowRedirects:     DefaultFollowRedirects,
 	})
 
 	return bow
