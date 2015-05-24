3b72138bb1cc4af63cee4d2d408bd49752dee460
headzoo <sean@headzoo.io>
2015-05-24 13:11:17 -0400

    Added test for Head method

--- surf_test.go
+++ surf_test.go
@@ -59,6 +59,22 @@ func TestPost(t *testing.T) {
 	ut.AssertEquals(200, bow.StatusCode())
 }
 
+func TestHead(t *testing.T) {
+	ut.Run(t)
+	var r *http.Request;
+	
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		r = req
+	}))
+	defer ts.Close()
+
+	bow := NewBrowser()
+
+	err := bow.Head(ts.URL + "/page1")
+	ut.AssertNil(err)
+	ut.AssertNotNil(r)
+}
+
 func TestDownload(t *testing.T) {
 	ut.Run(t)
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
